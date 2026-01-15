fced97101b47a777713c66bf05e5a864944967b3
fsoymaz <fthsymz60@gmail.com>
2026-01-15 23:05:41 +0300

    .

--- CalculatorLib/Calculator.cs
+++ CalculatorLib/Calculator.cs
@@ -4,7 +4,7 @@ namespace CalculatorLib
     {
         public int Add(int a, int b)
         {
-            return a + b;
+            return a - b;
         }
 
         public int Subtract(int a, int b)
