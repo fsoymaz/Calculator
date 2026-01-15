123ac870be59a3e7ce76d1c06d7a874a5d0d68f1
fsoymaz <fthsymz60@gmail.com>
2026-01-15 22:11:37 +0300

    Add Calculator library with Add and Subtract methods

---
+++ CalculatorLib/Calculator.cs
@@ -0,0 +1,15 @@
+namespace CalculatorLib
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
